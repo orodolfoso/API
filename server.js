1e104e0163fb0d063be69facc74356154e63c91a
orodolfoso <orodolfoso.dev@gmail.com>
2025-02-14 23:38:19 -0300

    API concluída

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -7,6 +8,7 @@ const prisma = new PrismaClient()
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 
 app.post('/users', async (req, res) => {
